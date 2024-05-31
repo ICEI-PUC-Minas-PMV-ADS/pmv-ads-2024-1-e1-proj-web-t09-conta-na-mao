7d4b2de3a91a5674173bb0998617d9189f869ce1
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-05-31 18:46:49 -0300

    cadastro de gasto no local storage

--- codigo-fonte/src/dashboard/adicionar-gasto/adicionarGasto.js
+++ codigo-fonte/src/dashboard/adicionar-gasto/adicionarGasto.js
@@ -4,17 +4,32 @@ const adicionarGasto = () => {
   let nomeGasto = document.getElementById("gasto").value;
   let categoriaGasto = document.getElementById("categoria").value;
   let valorGasto = document.getElementById("valor").value;
+  let tipo = categoriaGasto.toLowerCase().replace(" ", "-");
 
   let verificarCadastro = nomeGasto && categoriaGasto && valorGasto;
   let mensagemEnvio = document.getElementById("mensagemEnvio");
 
   if (verificarCadastro) {
-    listaDeContas.push({ nomeGasto, categoriaGasto, valorGasto });
-    // console.log(listaDeContas);
+    let novoGasto = {
+      nomeGasto,
+      categoriaGasto,
+      valorGasto,
+      tipo,
+    };
+    listaDeContas.push(novoGasto);
+    adicionarListaDeGastos(novoGasto);
 
     mensagemEnvio.innerHTML = "<span>Conta adicionada com sucesso!</span>";
   } else {
     mensagemEnvio.innerHTML =
       "<span>Todos os campos precisam ser preenchidos.</span>";
   }
+};
+
+const adicionarListaDeGastos = (novoGasto) => {
+  const listaDeGastosLocalStorage = localStorage.getItem("listaDeGastos");
+  const listaDeGastos = JSON.parse(listaDeGastosLocalStorage);
+
+  listaDeGastos.gastos.push(novoGasto);
+  localStorage.setItem("listaDeGastos", JSON.stringify(listaDeGastos));
 };
