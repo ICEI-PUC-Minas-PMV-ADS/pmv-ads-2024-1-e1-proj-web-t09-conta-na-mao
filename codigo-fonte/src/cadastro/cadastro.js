d5232c9f9745454f18bc535b48889263ca5221c3
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-06-02 18:51:57 -0300

    add menu lateral

--- codigo-fonte/src/cadastro/cadastro.js
+++ codigo-fonte/src/cadastro/cadastro.js
@@ -127,3 +127,20 @@ const postUsuario = (novoUsuario) => {
     )
     .catch((erro) => console.error("Erro: ", erro));
 };
+
+// ABRIR MENU
+
+const abrirMenu = () => {
+  const menuLateral = document.querySelector(".menu-lateral");
+  const menuIcone = document.querySelector(".icon-menu-principal");
+
+  if (menuLateral.classList.contains("menu-aberto")) {
+    menuLateral.classList.remove("menu-aberto");
+    menuLateral.classList.add("menu-fechado");
+    menuIcone.style.display = "block";
+  } else {
+    menuLateral.classList.remove("menu-fechado");
+    menuLateral.classList.add("menu-aberto");
+    menuIcone.style.display = "none";
+  }
+};
