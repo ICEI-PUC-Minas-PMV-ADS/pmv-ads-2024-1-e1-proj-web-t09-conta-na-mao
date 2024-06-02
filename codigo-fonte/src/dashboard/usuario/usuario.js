bbc3c3e14062f179eb3b5023d92eb7434995f40a
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-06-01 21:22:43 -0300

    add funções para exibir e modificar dados do usuário

--- codigo-fonte/src/dashboard/usuario/usuario.js
+++ codigo-fonte/src/dashboard/usuario/usuario.js
@@ -14,3 +14,63 @@ const abrirMenu = () => {
     menuIcone.style.display = "none";
   }
 };
+
+// PEGAR DADOS DO CLIENTE
+
+const getDadosDoCliente = () => {
+  fetch("http://localhost:3000/usuarios")
+    .then((resposta) => resposta.json())
+    .then((usuario) => {
+      const usuarioLocal = localStorage.getItem("usuarioLogado");
+      const usuarioJSON = JSON.parse(usuarioLocal);
+      const idDoUsuario = usuarioJSON.id;
+      const usuarioBanco = usuario[0].id;
+      if (idDoUsuario === usuarioBanco) gerarDados(usuario[0]);
+    })
+    .catch((erro) => console.error("Erro: ", erro));
+};
+
+const gerarDados = (usuario) => {
+  const usuarioNome = document.querySelector(".usuario-nome span");
+  const usuarioDados = document.querySelector(".usuario-dados");
+
+  usuarioNome.innerHTML = `${usuario.nome}`;
+
+  usuarioDados.innerHTML = `
+    <p>Email: <span>${usuario.email}</span></p>
+    <p>CPF: <span>${usuario.cpf}</span></p>
+    <p>Contato: <span>${usuario.contato}</span></p>
+    <p>Nascimento: <span>${usuario.dataNascimento}</span></p>
+  `;
+};
+
+getDadosDoCliente();
+
+// MUDAR SENHA
+
+const mudarSenha = () => {
+  alert("Essa opção está indisponível no momento.");
+};
+
+// EXCLUIR USUÁRIO
+
+const excluirUsuario = () => {
+  const usuario = localStorage.getItem("usuarioLogado");
+  const usuarioJSON = JSON.parse(usuario);
+  const idDoUsuario = usuarioJSON.id;
+
+  if (idDoUsuario) {
+    excluirUsuarioDoBanco(idDoUsuario);
+
+    localStorage.removeItem("usuarioLogado");
+    window.location.href = "../../index.html";
+  }
+};
+
+const excluirUsuarioDoBanco = (id) => {
+  fetch(`http://localhost:3000/usuarios?id=${id}`, {
+    method: "DELETE",
+  })
+    .then(console.log("Usuário excluído."))
+    .catch((erro) => console.error("Erro: ", erro));
+};
