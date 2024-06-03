a9aea11eff9a8d79ee6a118c28cf160ac37c69bf
AngeloAAO <145730747+AngeloAAO@users.noreply.github.com>
2024-06-03 10:21:56 -0400

    Update login.js
    Validação, testsar!

--- codigo-fonte/src/login/login.js
+++ codigo-fonte/src/login/login.js
@@ -19,6 +19,58 @@ const logarUsuario = () => {
     .catch((erro) => console.error("Erro: ", erro));
 };
 
+//Vaçidação
+
+function entrar(){
+  let usuario = document.querySelector('#usuario');
+  let userLabel = document.querySelector('#userLabel');
+  
+  let senha = document.querySelector('#senha');
+  let senhaLabel = document.querySelector('#senhaLabel');
+  
+  let msgError = document.querySelector('#msgError');
+  let listaUser = []
+  
+  let userValid = {
+    nome: '',
+    user: '',
+    senha: ''
+  }
+  
+  listaUser = JSON.parse(localStorage.getItem('listaUser'));
+  
+  listaUser.forEach((item) => {
+    if(usuario.value == item.userCad && senha.value == item.senhaCad){
+       
+      userValid = {
+         nome: item.nomeCad,
+         user: item.userCad,
+         senha: item.senhaCad
+       }
+      
+    }
+  })
+   
+  if(usuario.value == userValid.user && senha.value == userValid.senha){
+    window.location.href = '../../index.html'
+    
+    let mathRandom = Math.random().toString(16).substr(2);
+    let token = mathRandom + mathRandom;
+    
+    localStorage.setItem('token', token);
+    localStorage.setItem('userLogado', JSON.stringify(userValid));
+  } else {
+    userLabel.setAttribute('style', 'color: red');
+    usuario.setAttribute('style', 'border-color: red');
+    senhaLabel.setAttribute('style', 'color: red');
+    senha.setAttribute('style', 'border-color: red');
+    msgError.setAttribute('style', 'display: block');
+    msgError.innerHTML = 'Usuário ou senha incorretos'
+    usuario.focus();
+  }
+  
+}
+
 // ABRIR MENU
 
 const abrirMenu = () => {
